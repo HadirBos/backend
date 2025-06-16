84367d9d20d10f32c1290d36d767d276baf8ae75
FAISALAKBARr <faisalzogg022@gmail.com>
2025-06-16 08:39:04 +0700

    Fix main entry point in package.json and enhance MongoDB connection settings

--- src/config/db.js
+++ src/config/db.js
@@ -5,9 +5,12 @@ const connectDB = async () => {
         await mongoose.connect(process.env.DB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            serverSelectionTimeoutMS: 5000,
+            serverSelectionTimeoutMS: 30000,
             socketTimeoutMS: 45000,
             connectTimeoutMS: 60000,
+            heartbeatFrequencyMS: 2000,
+            retryWrites: true,
+            maxPoolSize: 50
         });
         console.log("MongoDB Connected...");
     } catch (error) {
