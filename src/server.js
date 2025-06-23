b348970aef3168ad906fd4945f5977cf8e64c101
rizkimaulana32 <112765218+rizkimaulana32@users.noreply.github.com>
2025-06-23 22:43:00 +0700

    fix: bug patch

--- src/server.js
+++ src/server.js
@@ -35,7 +35,7 @@ app.use(cors({
     'http://localhost:5173' 
   ],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
